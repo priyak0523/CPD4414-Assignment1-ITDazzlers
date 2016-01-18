57a26d5b5b0d4f50cb15651dafa7acbd4e8ceb75
c0664573 <c0664573@N208-F15-006.lambton.on.ca>
2016-01-18 15:57:34 -0500

    Initial Commit

---
+++ src/cpd4414/assignment1/itdazzlers/CPD4414Assignment1ITDazzlers.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment1.itdazzlers;
+
+/**
+ *
+ * @author c0664573
+ */
+public class CPD4414Assignment1ITDazzlers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
